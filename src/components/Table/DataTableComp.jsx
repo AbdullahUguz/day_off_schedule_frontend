fd75f8df9a98c775f8f24e8ea02e60895726f98a
AbdullahUguz <abdullahuguz08@gmail.com>
2023-08-04 17:01:56 +0300

    delete alert changed

--- src/components/Table/DataTableComp.jsx
+++ src/components/Table/DataTableComp.jsx
@@ -84,26 +84,39 @@ function DataTableComp({ employees, control, setControl }) {
       cell: (row) =>
 
         <Button variant="danger" onClick={async () => {
-          await fetchDeleteEmployee({
-            employeeId: row.id
-          }).then(res => {
-            console.log(res);
-            setControl(!control);
-            Swal.fire({
-              icon: 'success',
-              title: 'Employee has been deleted',
-              showConfirmButton: false,
-              timer: 1100
-            })
-          }).catch(err => {
-            Swal.fire({
-              icon: 'error',
-              title: 'There is a problem',
-              showConfirmButton: false,
-              timer: 1100
-            })
-            console.log("delete: ", err)
+
+          Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              await fetchDeleteEmployee({
+                employeeId: row.id
+              }).then(res => {
+                console.log(res);
+                setControl(!control);
+              }).catch(err => {
+                Swal.fire({
+                  icon: 'error',
+                  title: 'There is a problem',
+                  showConfirmButton: false,
+                  timer: 1100
+                })
+                console.log("delete: ", err)
+              })
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+            }
           })
+          
         }}>Delete</Button>
       ,
       ignoreRowClick: true,
