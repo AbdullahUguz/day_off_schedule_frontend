b8f4dcb86e1f38afe453769c23c598f5b3b0f96d
AbdullahUguz <abdullahuguz08@gmail.com>
2023-08-21 15:41:07 +0300

    edit day off modal validation updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-//--- employees
+//--- employees ---
 
 export const fetchRegister = async (input) => {
-  const { data } = await axios.post(`http://localhost:8080/employees/create`, input);
+  const { data } = await axios.post(
+    `http://localhost:8080/employees/create`,
+    input
+  );
   return data;
 };
 
@@ -13,53 +16,59 @@ export const fetchGetAllEmployee = async () => {
 };
 
 export const fetchUpdateEmployee = async (input) => {
-  const { data } = await axios.put(`http://localhost:8080/employees/update/${input.employeeId}`,input);
+  const { data } = await axios.put(
+    `http://localhost:8080/employees/update/${input.employeeId}`,
+    input
+  );
   return data;
 };
 
-//--- day Off---
+//--- day Off ---
 export const fetchGetDayOffById = async (input) => {
-  const { data } = await axios.get(`http://localhost:8080/daysOff/getDayOff/${input.employeeId}`);
+  const { data } = await axios.get(
+    `http://localhost:8080/daysOff/getDayOff/${input.employeeId}`
+  );
   return data;
 };
 
-export const fetchAddDayOffDetail = async (input)=>{
-  const { data } = await axios.post(`http://localhost:8080/daysOff/addDayOffDetail/${input.dayOffId}`,{
-    startDate:new Date(input.dayOffDetail.startDate).toISOString().split('T')[0],
-    endDate:new Date(input.dayOffDetail.endDate).toISOString().split('T')[0],
-    explanation:input.dayOffDetail.explanation,
-    usedDayOff:input.dayOffDetail.usedDayOff
-  });
-  console.log("sss: ",input.dayOffDetail)
-  return data;
-}
-
-
-export const fetchEditEmployeeRemainingDayOff = async (input) => {
-  const { data } = await axios.put(`http://localhost:8080/employees/remainingDayOff/${input.employeeId}`,{
-    usedDayOff:input.usedDayOff
-  });
+export const fetchAddDayOffDetail = async (input) => {
+  const { data } = await axios.post(
+    `http://localhost:8080/daysOff/addDayOffDetail/${input.dayOffId}`,
+    {
+      startDate: new Date(input.dayOffDetail.startDate).toISOString().split("T")[0],
+      endDate: new Date(input.dayOffDetail.endDate).toISOString().split("T")[0],
+      explanation: input.dayOffDetail.explanation,
+      usedDayOff: input.dayOffDetail.usedDayOff,
+    }
+  );
   return data;
 };
-
-export const fetchEmailControl = async (input) => {
-  const { data } = await axios.post(`http://localhost:8080/employees/isEmailExist`,input);
-  return data;
-};
-
-
-
-export const fetchDeleteEmployee = async (input) => {
-  const { data } = await axios.delete(`http://localhost:8080/employees/delete/${input.employeeId}`);
-  return data;
-};
-
 
 export const fetchResetRemainingDayOff = async (input) => {
-  const { data } = await axios.post(`http://localhost:8080/employees/resetRemainingDayOff/${input.employeeId}`);
+  const { data } = await axios.post(
+    `http://localhost:8080/daysOff/resetRemainingDayOff/${input.dayOffId}`
+  );
   return data;
 };
 
+export const fetchDeleteEmployee = async (input) => {
+  const { data } = await axios.delete(
+    `http://localhost:8080/employees/delete/${input.employeeId}`
+  );
+  return data;
+};
+
+// export const fetchEditEmployeeRemainingDayOff = async (input) => {
+//   const { data } = await axios.put(`http://localhost:8080/employees/remainingDayOff/${input.employeeId}`,{
+//     usedDayOff:input.usedDayOff
+//   });
+//   return data;
+// };
+
+// export const fetchEmailControl = async (input) => {
+//   const { data } = await axios.post(`http://localhost:8080/employees/isEmailExist`,input);
+//   return data;
+// };
 
 // --- departments ---
 export const fetchGetAllDepartment = async () => {
@@ -68,6 +77,9 @@ export const fetchGetAllDepartment = async () => {
 };
 
 export const fetchCreateDepartment = async (input) => {
-  const { data } = await axios.post(`http://localhost:8080/departments/create`, input);
+  const { data } = await axios.post(
+    `http://localhost:8080/departments/create`,
+    input
+  );
   return data;
 };
